9fa02970cf2571d94e309c4069eaf07841145f58
rshkabko <r.shkabko@flamix.email>
2023-06-30 00:34:11 +0300

    Check plugin in network

--- src/General/Checker.php
+++ src/General/Checker.php
@@ -14,10 +14,11 @@ class Checker
     {
         $result = is_bool($callback) ? $callback : (bool)$callback();
 
-        if ($result)
+        if ($result) {
             $result = '<span style="color: #46b450;">' . ($options['0'] ?? 'Enable') . '</span>';
-        else
+        } else {
             $result = '<span style="color: #dc3232;">' . ($options['1'] ?? 'Disabled') . '</span>';
+        }
 
         return $name . ': ' . $result;
     }
@@ -33,7 +34,16 @@ class Checker
     public static function isPluginActive(string $plugin)
     {
         $plugin = str_contains($plugin, '.php') ? $plugin : "{$plugin}/{$plugin}.php";
-        return in_array($plugin, (array)get_option('active_plugins', []));
+
+        $wordpress_plugins = (array)get_option('active_plugins', []);
+
+        // Multisite
+        if (MULTISITE) {
+            $network_plugins = array_keys((array)get_site_option('active_sitewide_plugins', []));
+            $wordpress_plugins = array_merge($wordpress_plugins, $network_plugins);
+        }
+
+        return in_array($plugin, $wordpress_plugins);
     }
 
     /**
@@ -44,9 +54,6 @@ class Checker
      */
     public static function isEmail(string $option)
     {
-        if (filter_var($option, FILTER_VALIDATE_EMAIL))
-            return $option;
-
-        return false;
+        return filter_var($option, FILTER_VALIDATE_EMAIL) ? $option : false;
     }
 }
